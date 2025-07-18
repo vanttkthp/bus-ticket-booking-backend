d40e38b75e45b7a702d22eeece1879daef757daa
van2k2dev <van02.dao@lge.com>
2025-07-18 17:52:00 +0700

    build code base

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User, UserDocument } from './schemas/user.schema';
@@ -8,10 +8,22 @@ import { CreateUserDto } from './dto/create-user.dto';
 export class UsersService {
   constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
 
-  async create(createUserDto: CreateUserDto) : Promise<User> {
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    const existingUser = await this.userModel.findOne({
+      $or: [{ phone: createUserDto.phone }, { email: createUserDto.email }],
+    });
+
+    if (existingUser) {
+      throw new HttpException(
+        'User with this phone or email already exists',
+        HttpStatus.CONFLICT,
+      );
+    }
+
     const createdUser = new this.userModel(createUserDto);
     return createdUser.save();
   }
+
   async findAll(): Promise<User[]> {
     return this.userModel.find().exec();
   }
